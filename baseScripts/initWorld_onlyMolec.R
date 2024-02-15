618ee4ce642529dfc87469ad9920dddaca3b28f1
Matthias Hof <hofm@rivm-rsrv-l02p.rivm.ssc-campus.nl>
2024-02-15 15:43:27 +0100

    Added folder in Vignettes/Development for Quality Control scripts: scripts for testing/ record taking of error solving or other quality control

--- baseScripts/initWorld_onlyMolec.R
+++ baseScripts/initWorld_onlyMolec.R
@@ -64,15 +64,15 @@ World$PostponeVarProcess(VarFunctions = "OtherkAir", ProcesFunctions = "k_Deposi
 
 #verbose = T
 
-#kex = World$NewCalcVariable("rad_species")
-#kex$execute(debugAt = list(SubCompartName = "air"))
+# kex = World$NewCalcVariable("rad_species")
+# kex$execute(debugAt = list(SubCompartName = "air"))
 verbose = T
 World$UpdateKaas()
 
-emissions <- data.frame(Abbr = "aRU", Emis = 1000)
-
-World$NewSolver("SB1Solve")
-World$Solve(emissions)
-#World$states$sortFactors(emissions)
-
-World$states$sortFactors(World$fetchData("EqMass"))
+# emissions <- data.frame(Abbr = "aRU", Emis = 1000)
+# 
+# World$NewSolver("SB1Solve")
+# World$Solve(emissions)
+# #World$states$sortFactors(emissions)
+# 
+# World$states$sortFactors(World$fetchData("EqMass"))
