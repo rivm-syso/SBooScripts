910149fd2511677ba5beae3e7cac45651cf14dd8
Jaap Slootweg <slootwej@rivm-lsfsv-l13p.rivm.ssc-campus.nl>
2024-06-24 17:40:43 +0200

    mutateVars demonstrated

--- testScripts/TestMutateVar.R
+++ testScripts/TestMutateVar.R
@@ -2,6 +2,9 @@
 #create World as SBcore
 source("baseScripts/initWorld_onlyParticulate.R")
 
+# mutate Vars, 2 ways: a file or from a list of dataframes
+World$mutateVars("testScripts/solvError.csv")
+
 TotalArea <- data.frame(
   Scale = c("Arctic", "Continental", "Moderate", "Regional"),
   TotalArea = c(4.25E+13, 7.43E+12, 8.50E+13, 4.13e+11)
@@ -16,10 +19,18 @@ RAINrate <- data.frame(
   Scale = "Arctic",
   RAINrate = 4.37e-5
 )
-dfs <- list(TotalArea, Temperature, RAINrate)
-for (i in seq_along(dfs)) {
-  dfs[[i]] <- World$mutateVar(dfs[[i]])
-}
+
+ToPaste <- lapply(list(TotalArea, Temperature, RAINrate), function(x) {
+  varName <- names(x)[!names(x) %in% The3D]
+  stopifnot(length(varName)==1)
+  # one line with 2 disadvantages of tidyverse..:
+  as.data.frame(pivot_longer(data = x, cols = all_of(varName), names_to = "varName", values_to = "Waarde"))
+})
+#and one advantage: bind_rows handles missing columns
+dfs <- do.call(bind_rows, ToPaste)
+
+World$mutateVars(dfs)
+
 #add emission and the solver for uncertainty
 emissions <- data.frame(Abbr = "aRU", Emis = 1)
 World$NewSolver("vUncertain")
