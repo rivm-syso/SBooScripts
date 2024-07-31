e311c23d4f106489ac534dc142751de8f99a8410
Jaap Slootweg <jaap.slootweg@rivm.nl>
2024-07-31 14:58:28 +0200

    labour to update the datalayer if a substance is set. Rename COL -> coloidConc

--- baseScripts/initWorld_onlyParticulate.R
+++ baseScripts/initWorld_onlyParticulate.R
@@ -5,18 +5,19 @@ source("baseScripts/fakeLib.R")
 
 #to run the script with another selection of substance / excel reference, #
 #set the variables substance and excelReference before sourcing this script, like substance = "nAg_10nm"
-if (!exists("substance")) {
-  substance <- "nAg_10nm"
-}
+#   no longer needed
+#if (!exists("substance")) {
+#  substance <- "nAg_10nm"
+#}
 #The script creates the "ClassicStateModule" object with the states of the classic 4. excel version. 
-ClassicStateModule <- ClassicNanoWorld$new("data", substance)
+ClassicStateModule <- ClassicNanoWorld$new("data")
 
 #with this data we create an instance of the central "core" object,
 World <- SBcore$new(ClassicStateModule)
-# We are interested in the particulate species only, so no need to filter like in the Molecular initWorld
 
 # To proceed with testing we set
 
+World$substance <- "nAg_10nm"
 if (is.na(World$fetchData("pKa"))) {
   warning("pKa is needed but missing, setting pKa=7")
   World$SetConst(pKa = 7)
@@ -32,8 +33,6 @@ if (is.na(World$fetchData("Pvap25"))) {
   World$SetConst(Pvap25 = 1e-7)
 }
 World$SetConst(DragMethod = "Original")
-AllF <- ls() %>% sapply(FUN = get)
-ProcessDefFunctions <- names(AllF) %>% startsWith("k_")
 
 #call the particulate processes 
 Processes4SpeciesTp <- read.csv("data/Processes4SpeciesTp.csv")
@@ -41,8 +40,8 @@ ParProcesses <- Processes4SpeciesTp$Process[grepl("[a-z,A-Z]", Processes4Species
 sapply(paste("k", ParProcesses, sep = "_"), World$NewProcess)
 
 #add all flows, they are all part of "Advection"
-FluxDefFunctions <- names(AllF) %>% startsWith("x_")
-sapply(names(AllF)[FluxDefFunctions], World$NewFlow)
+FluxDefFunctions <- ls(pattern = "x_")
+sapply(FluxDefFunctions, World$NewFlow)
 World$SetConst(Test = "TRUE")
 #derive needed variables
 World$VarsFromprocesses()
