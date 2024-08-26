2047f504bff5e8d29659446f8d52b2fc88151d6e
Valerie de Rijk <rijkdv@rivm-lsfsv-l13p.rivm.ssc-campus.nl>
2024-08-26 15:45:05 +0200

    dynamic computation test

--- testScripts/TestConcentrationR6.R
+++ testScripts/TestConcentrationR6.R
@@ -17,3 +17,21 @@ solved2 <- World$Solve(emissions, needdebug = F)
 sol <- World$Solution()
 World$GetConcentration()
 
+#### Test for dynamic 
+substance <- "microplastic"
+source("baseScripts/initWorld_onlyPlastics.R")
+World$substance <- substance
+
+# Voorbeeld met dataframe
+emissions <- data.frame(Abbr = c("aRS", "s2RS", "w1RS","aRS", "s2RS", "w1RS"), Emis = c(10, 10, 10,20, 20, 20), Timed = c(1, 2, 3, 4, 5, 6)) # convert 1 t/y to si units: kg/s
+
+emissions <- emissions |>
+  mutate(Timed = Timed*(365.25*24*60*60)) |> ungroup()
+
+tmax <- 365.25*24*60*60*10
+times <- seq(0, tmax, length.out = 10)
+
+World$NewSolver("DynApproxSolve")
+solved <- World$Solve(tmax = tmax, emissions, needdebug = F)
+
+Conc <- World$GetConcentration()
