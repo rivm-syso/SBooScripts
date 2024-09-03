1803cb220ee3faf50bba20f2bcff93368b2f9316
anne-hids <anne.hids@rivm.nl>
2024-09-03 10:26:28 +0200

    updating test scripts

--- testScripts/testUncertainDynamicSolver.R
+++ testScripts/testUncertainDynamicSolver.R
@@ -4,15 +4,136 @@ library(tidyverse)
 #create World as SBcore
 source("baseScripts/initWorld_onlyMolec.R")
 
-emissions <- data.frame(Abbr = c("aRU", "s2RU", "w1RU"), Emis = c(10000, 10000, 10000)) # convert 1 t/y to si units: kg/s
-
-MW <- World$fetchData("MW")
+########################## Single emission df example ##########################
+# Make emission df with timed column
+emissions <- data.frame(Abbr = c("aRU", "s2RU", "w1RU","aRU", "s2RU", "w1RU"), Emis = c(10, 10, 10,20, 20, 20), Timed = c(1, 2, 3, 4, 5, 6)) # convert 1 t/y to si units: kg/s
 
 emissions <- emissions |>
+  mutate(Timed = Timed*(365.25*24*60*60)) |> ungroup() |>
   mutate(Emis = Emis*1000/(365*24*60*60)) 
 
+tmax <- 365.25*24*60*60*10
+times <- seq(0, tmax, length.out = 10)
+
+# Make variable input 
 # Define the number of samples and the number of variables
-n_samples <- 4
+n_samples <- 10
+n_vars <- 3
+
+# Generate LHS
+lhs_samples <- randomLHS(n_samples, n_vars)
+
+# Define triangular distribution function
+triangular_cdf_inv <- function(u, a, b, c) {
+  ifelse(u < (c-a)/(b-a),
+         a + sqrt(u * (b-a) * (c-a)),
+         b - sqrt((1-u) * (b-a) * (b-c)))
+}
+
+# Define the names of the uncertain variables
+var1Name <- "Area"
+
+var1 <- World$fetchData(var1Name) |>
+  filter(Scale == "Regional") |>
+  filter(SubCompart == "sea")
+
+# Set the parameters for the triangular distribution
+var1$a <- var1$Area*0.7    # Minimum value
+var1$b <- var1$Area*1.3    # Maximum value
+var1$c <- var1$Area         # Mode (peak)
+
+# Define the names of the uncertain variables
+var2Name <- "Area"
+
+var2 <- World$fetchData(var2Name) |>
+  filter(Scale == "Regional") |>
+  filter(SubCompart == "river")
+
+# Set the parameters for the triangular distribution
+var2$a <- var2$Area*0.7    # Minimum value
+var2$b <- var2$Area*1.3    # Maximum value
+var2$c <- var2$Area         # Mode (peak)
+
+
+# Define the names of the uncertain variables
+var3Name <- "EROSIONsoil"
+
+var3 <- World$fetchData(var3Name) |>
+  filter(SubCompart == "agriculturalsoil") |>
+  mutate(Scale = NA)
+
+# Set the parameters for the triangular distribution
+var3$a <- var3$EROSIONsoil*0.7    # Minimum value
+var3$b <- var3$EROSIONsoil*1.3    # Maximum value
+var3$c <- var3$EROSIONsoil         # Mode (peak)
+
+params <- tibble(
+  varName = c(var1Name, var2Name, var3Name),
+  Scale = c(var1$Scale, var2$Scale, var3$Scale),
+  SubCompart = c(var1$SubCompart, var2$SubCompart, var3$SubCompart),
+  data = list(
+    tibble(id = c("a", "b", "c"), value = c(var1$a, var1$b, var1$c)),
+    tibble(id = c("a", "b", "c"), value = c(var2$a, var2$b, var2$c)),
+    tibble(id = c("a", "b", "c"), value = c(var3$a, var3$b, var3$c))
+  )
+)
+
+sample_df <- params
+
+# Transform each LHS sample column to the corresponding triangular distribution
+for (i in 1:n_vars) {
+  a <- filter(params$data[[i]], id == "a") %>% pull(value)
+  b <- filter(params$data[[i]], id == "b") %>% pull(value)
+  c <- filter(params$data[[i]], id == "c") %>% pull(value)
+  
+  samples <- triangular_cdf_inv(lhs_samples[, i], a, b, c)
+  
+  # Create a new tibble for 'data' with samples replacing original values
+  new_data <- tibble(value = samples)
+  
+  # Update the data column in the sample_df
+  sample_df$data[[i]] <- new_data
+}
+
+# Solve with dynamic uncertain solver
+# World$NewSolver("UncertainDynamicSolver")
+# solved <- World$Solve(emissions, sample_df, tmax = tmax, needdebug = T)
+
+# sol1 <- solved$Mass[[1]]
+############################ Test with one funlist as input ####################
+
+SBEmissions3 <- 
+  emissions |> 
+  group_by(Abbr) |> 
+  summarise(n=n(),
+            EmisFun = list(
+              approxfun(
+                data.frame(Timed = c(0,Timed), 
+                           Emis=c(0,Emis)),
+                rule = 2) # Change to rule 1:1 for no extrapolation
+            )
+  )
+
+funlist <- SBEmissions3$EmisFun
+names(funlist) <- SBEmissions3$Abbr
+
+tmax <- 365.25*24*60*60*10
+times <- seq(0, tmax, length.out = 10)
+
+World$NewSolver("UncertainDynamicSolver")
+solved <- World$Solve(funlist, sample_df, tmax = tmax, needdebug = T)
+
+
+
+
+
+
+
+
+
+
+# Define the number of samples and the number of variables
+n_samples <- 10
 n_vars <- 3
 n_comps <- 2
 
@@ -38,7 +159,7 @@ var1Name <- "Area"
 var1 <- World$fetchData(var1Name) |>
   filter(Scale == "Regional") |>
   filter(SubCompart == "sea")
-  
+
 # Set the parameters for the triangular distribution
 var1$a <- var1$Area*0.7    # Minimum value
 var1$b <- var1$Area*1.3    # Maximum value
@@ -154,34 +275,5 @@ for (i in 1:n_comps) {
 }
 
 ################################## Solve #######################################
-
-World$NewSolver("UncertainSolver")
+World$NewSolver("UncertainDynamicSolver")
 solved <- World$Solve(emissions, needdebug = T, sample_df)
-
-
-
-
-
-
-
-
-######################### Find way to reach the values again ###################
-
-# Get all values corresponding to a var
-nrow(sample_df$data[1])
-
-# Get one instance
-nrow(sample_df$data[[1]])
-
-# Get first value of first df in data
-first_value <- map(emis_df$Emis[1], ~ .x$value[1])[[1]]
-
-# Get first value of all dfs in data
-first_values <- map(emis_df$Emis, ~ .x$value[1])
-
-first_values <- map_dfr(emis_df$Emis, ~ tibble(Emis = .x$value[1]))
-
-## Test if the data really is triangularly distributed
-test <- sample_df$data[[1]]
-hist(test$value)
-
