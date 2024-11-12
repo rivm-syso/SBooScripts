4bf09833ccdf43665cd5ffaac84a375408eb1f2e
anne-hids <anne.hids@rivm.nl>
2024-11-12 11:57:25 +0100

    Tried to make this work, but it doesn't yet

--- vignettes/CaseStudies/batch_temp.R
+++ vignettes/CaseStudies/batch_temp.R
@@ -1,32 +1,30 @@
-## Selects a variety of parameter combinations to run
+library(batch)
+library(tidyverse)
 
-library("batch")
-
-## Choose a high enough seed for later for pasting the results together
-##  (1,...,9,10) sorts not the way you want, for example.
-## Here setting any parameter=value will override the default in the R file
-##  you are running
-
+## Choose a high enough seed for sequential organization
 seed <- 20001
 
-batch_n = 2
-batch_max = 8 # should be multiple of batch_n
+## Define batch parameters
+batch_n <- 2
+batch_max <- 1000  # Should be a multiple of batch_n for the loop logic to work
 
+## Create parameter grid
 pars <- expand.grid(
-  RUN1=seq(batch_n,batch_max,batch_n) #,
-#  material = c("NR","SBR") # now not used, but can also be part of batch
-)
-library(tidyverse)
-pars <-
-  pars |> mutate(RUN2 = RUN1-(batch_n-1))
+  RUN1 = seq(batch_n, batch_max, batch_n)
+) %>%
+  mutate(RUN2 = RUN1 - (batch_n - 1))
 
-for(i in 1:length(pars[,1]))
+## Run batch jobs
+for(i in 1:nrow(pars)) {
+  # Define the sequence for RUNSamples based on RUN2 and RUN1
+  RUNSamples <- pars$RUN2[i]:pars$RUN1[i]
+  
+  # Call rbatch with specific parameters
   seed <- rbatch("vignettes/CaseStudies/03_get_Solution_SB_batch.r", 
                  seed = seed, 
-                 RUNSamples = c(pars$RUN1[i]:pars$RUN2[i])
-                 )
-
-## Only for local (but it does not hurt to run in other situations,
-##  so suggested in all cases).
-## This actually runs all the commands when run on the local system.
-rbatch.local.run(ncores=4)
+                 RUNSamples = RUNSamples
+  )
+  
+  # Increment seed to ensure unique seeds for each job if needed
+  seed <- seed + 1
+}
