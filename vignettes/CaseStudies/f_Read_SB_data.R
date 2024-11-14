fbe78f87fd58b299a003df1595cb2e31c59731d9
anne-hids <anne.hids@rivm.nl>
2024-11-14 11:32:36 +0100

    Made function for reading in SB output Rdata files

---
+++ vignettes/CaseStudies/f_Read_SB_data.R
@@ -0,0 +1,58 @@
+# Function to process a filepath and extract the SimpleBox outcome in the correct format
+process_filepath <- function(filepath) {
+  load(filepath)
+  
+  # Extract run values
+  runs <- str_extract(filepath, "(?<=_RUNS_)[0-9_]+")
+  runs <- unlist(str_split(runs, "_"))
+  minrun <- as.integer(runs[1])
+  maxrun <- as.integer(runs[2])
+  new_run_values <- seq(minrun, maxrun)
+  
+  # Process material parameters
+  Material_Parameters_n <- Material_Parameters_n |>
+    select(VarName, Source, Scale, SubCompart, Species, Distribution, Polymer, Unit, data) |>
+    mutate(data = lapply(data, function(df) {
+      df$RUN <- new_run_values
+      return(df)
+    })) |>
+    unnest(data)
+  
+  # Initialize output lists
+  output_data <- list(
+    TW_concentrations = tibble(),
+    TW_solutions = tibble(),
+    Other_concentrations = tibble(),
+    Other_solutions = tibble(),
+    Material_Parameters_long = Material_Parameters_n,
+    Units = Units
+  )
+  
+  # Process outcomes
+  for (j in seq_along(Output$SBoutput)) {
+    polymer <- Output$Polymer[j]
+    polymer_outcome <- Output$SBoutput[[j]]
+    
+    conc <- polymer_outcome$DynamicConc$Concentrations |>
+      mutate(RUN = new_run_values[as.integer(RUN)], Polymer = polymer)
+    
+    sol <- polymer_outcome$DynamicMass |>
+      mutate(RUN = new_run_values[as.integer(RUN)], Polymer = polymer)
+    
+    if ("NR" %in% unique(Output$Polymer)) {
+      conc <- conc |> mutate(Source = "Tyre wear")
+      sol <- sol |> mutate(Source = "Tyre wear")
+      
+      output_data$TW_concentrations <- bind_rows(output_data$TW_concentrations, conc)
+      output_data$TW_solutions <- bind_rows(output_data$TW_solutions, sol)
+    } else {
+      conc <- conc |> mutate(Source = "Other sources")
+      sol <- sol |> mutate(Source = "Other sources")
+      
+      output_data$Other_concentrations <- bind_rows(output_data$Other_concentrations, conc)
+      output_data$Other_solutions <- bind_rows(output_data$Other_solutions, sol)
+    }
+  }
+  
+  return(output_data)
+}
