3c27a28ec5eda0d6091f654946b069e967f65528
anne-hids <anne.hids@rivm.nl>
2024-11-21 09:12:48 +0100

    Added lines for saving emission data

--- vignettes/CaseStudies/f_Read_SB_data.R
+++ vignettes/CaseStudies/f_Read_SB_data.R
@@ -22,8 +22,10 @@ process_filepath <- function(filepath) {
   output_data <- list(
     TW_concentrations = tibble(),
     TW_solutions = tibble(),
+    TW_emissions = tibble(),
     Other_concentrations = tibble(),
     Other_solutions = tibble(),
+    Other_emissions = tibble(),
     Material_Parameters_long = Material_Parameters_n,
     Units = tibble(),
     States = tibble()
@@ -40,21 +42,29 @@ process_filepath <- function(filepath) {
     sol <- polymer_outcome$DynamicMass |>
       mutate(RUN = new_run_values[as.integer(RUN)], Polymer = polymer)
     
+    emis <- polymer_outcome$Input_Emission |>
+      unnest(Emis) |>
+      mutate(RUN = new_run_values[as.integer(RUN)], Polymer = polymer)
+    
     output_data$Units <- polymer_outcome$DynamicConc$Units
     output_data$States <- polymer_outcome$States
     
     if ("NR" %in% unique(Output$Polymer)) {
       conc <- conc |> mutate(Source = "Tyre wear")
       sol <- sol |> mutate(Source = "Tyre wear")
+      emis <- emis |> mutate(Source = "Tyre wear")
       
       output_data$TW_concentrations <- bind_rows(output_data$TW_concentrations, conc)
       output_data$TW_solutions <- bind_rows(output_data$TW_solutions, sol)
+      output_data$TW_emissions <- bind_rows(output_data$TW_emissions, emis)
     } else {
       conc <- conc |> mutate(Source = "Other sources")
       sol <- sol |> mutate(Source = "Other sources")
+      emis <- emis |> mutate(Source = "OTher sources")
       
       output_data$Other_concentrations <- bind_rows(output_data$Other_concentrations, conc)
       output_data$Other_solutions <- bind_rows(output_data$Other_solutions, sol)
+      output_data$Other_emissions <- bind_rows(output_data$Other_emissions, emis)
     }
   }
   
