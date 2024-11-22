35824ba3cd90b7daf439f2b52435c9e4eae12b16
anne-hids <anne.hids@rivm.nl>
2024-11-22 12:22:24 +0100

    Save the DPMFA data files separately to take up less memory when loading data

--- vignettes/CaseStudies/06_save_emission_figure_data.R
+++ vignettes/CaseStudies/06_save_emission_figure_data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 data_folder <-  "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/"
 
+########################## Load and prepare the data ###########################
 MFAtype <- "DPMFA"
 
 load(file = paste0(data_folder, MFAtype, "_EU", ".RData"))
@@ -14,8 +15,7 @@ data_long_EU <-
   pivot_longer(cols=-c(Type, Scale, Source, Polymer, To_Compartment, Material_Type, iD_source, RUN),
                names_to = "Year",
                values_to = "Mass_Polymer_kt") |>
-  select(-c(Scale, iD_source)) |>
-  filter(Year = year)
+  select(-c(Scale, iD_source)) 
 
 load(file = paste0(data_folder, MFAtype, "_NL", ".RData"))
 
@@ -26,8 +26,7 @@ data_long_NL <-
   pivot_longer(cols=-c(Type, Scale, Source, Polymer, To_Compartment, Material_Type, iD_source, RUN),
                names_to = "Year",
                values_to = "Mass_Polymer_kt") |>
-  select(-c(Scale, iD_source)) |>
-  filter(Year = year)
+  select(-c(Scale, iD_source)) 
 
 sinks <- unique(c(sinks_EU, sinks_NL))
 
@@ -36,9 +35,37 @@ data_long <- data_long_EU |>
   mutate(Mass_Polymer_kt = replace_na(Mass_Polymer_kt.x, 0) + replace_na(Mass_Polymer_kt.y, 0)) |> # Account for cases where Mass_Polymer_kt is NA
   select(-c(Mass_Polymer_kt.x, Mass_Polymer_kt.y)) |>
   mutate(Scale = "EU") |>
-  filter(To_Compartment %in% sinks & Type == "Inflow")
+  filter(To_Compartment %in% sinks & Type == "Inflow") 
 
-save(data_long, data_long_EU, data_long_NL,
-     file = "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/DPMFA_emission_data.RData",
+############################ Make datasets to save #############################
+TW_emissions_over_time <- data_long |>
+  filter(Source == "Tyre wear")
+
+data_long <- data_long |>
+  filter(Year == year)
+
+data_long_NL <- data_long_NL |>
+  filter(To_Compartment %in% sinks & Type == "Inflow") |>
+  filter(Year == year)
+
+data_long_EU <-data_long_EU |>
+  filter(To_Compartment %in% sinks & Type == "Inflow") |>
+  filter(Year == year)
+
+################################ Save datasets #################################
+save(data_long, 
+     file = "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/DPMFA_NL_EU_long.RData",
+     compress = "xz",
+     compression_level = 9) 
+save(data_long_EU,
+     file = "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/DPMFA_NL_long.RData",
+     compress = "xz",
+     compression_level = 9) 
+save(data_long_NL,
+     file = "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/DPMFA_EU_long.RData",
+     compress = "xz",
+     compression_level = 9) 
+save(TW_emissions_over_time,
+     file = "/data/BioGrid/hidsa/SimpleBox/SBooScripts/vignettes/CaseStudies/CaseData/DPMFA_emission_NL_EU_TW_time.RData",
      compress = "xz",
      compression_level = 9) 
