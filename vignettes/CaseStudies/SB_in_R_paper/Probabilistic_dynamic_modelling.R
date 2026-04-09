1f03d0f89297b631611ba791fce818203c38d247
anne-hids <anne.hids@rivm.nl>
2026-04-09 13:18:06 +0200

    Remove unused files

--- vignettes/CaseStudies/SB_in_R_paper/Probabilistic_dynamic_modelling.R
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Script for making dynamic probabilistic calculations with SimpleBox          #
-# Created for SimpleBox in R paper                                             #
-# Authors: Anne Hids and Joris Quik                                            #
-# RIVM                                                                         #
-# 8-1-2024                                                                    #
-################################################################################
-
